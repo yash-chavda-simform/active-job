e03851d092ecba1fe1e62f56f845f30b9d61251e
yash <yash.c@simformsolutions.com>
2023-05-15 14:20:38 +0530

    TE7-T133:ActiveJob:fix:changes has been done

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+  default from: 'demo42424242@gmail.com'
   def welcome_email(user)
     @user = user
     mail(to: @user.email, subject: "User Is Created.")
@@ -7,14 +7,14 @@ class UserMailer < ApplicationMailer
 
   def send_event_list(events)
     @events = events
-    mail(to: -> { User.pluck(:email) }, subject: "Event List")
+    mail(to: User.pluck(:email), subject: "Event List")
   end
 
   def send_reminder_email(events)
     @events = events 
     @events.each do |event|
       @current_event = event
-      mail(to: -> { event.users.pluck(:email) }, subject: "Event Reminder")
+      mail(to: event.users.pluck(:email), subject: "Event Reminder")
     end
   end
 end
--- app/sidekiq/create_event_job.rb
+++ app/sidekiq/create_event_job.rb
@@ -2,7 +2,7 @@ class CreateEventJob
   include Sidekiq::Job
 
   def perform
-    last_login = DateTime.parse('2023-04-14 16:30:00')
-    Event.create(name: Faker::Lorem.words(number: 3).join(" "), category: Faker::Lorem.word, entry_fees: Faker::Number.number(digits: 3), starting_time: last_login)
+    date_event = DateTime.parse('2023-04-14 16:30:00')
+    Event.create(name: Faker::Lorem.words(number: 3).join(" "), category: Faker::Lorem.word, entry_fees: Faker::Number.number(digits: 3), starting_time: date_event)
   end
 end
